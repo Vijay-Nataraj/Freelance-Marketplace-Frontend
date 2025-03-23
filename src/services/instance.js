fb387b66ed3ffbf569b5c685023dd99283cf5baf
Vijay-Nataraj <victoryvijayn@gmail.com>
2025-03-23 08:16:50 +0530

    baseURL updated

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3000/api/v1";
+const baseURL = "https://freelance-marketplace-backend.onrender.com/api/v1";
 
 const instance = axios.create({
   baseURL,
